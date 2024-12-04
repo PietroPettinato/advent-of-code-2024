7da44e316600a72c156439acc6f2123c66e35907
Pietro Francesco Pettinato <piepettinato@gmail.com>
2024-12-04 17:08:28 +0100

    day 1 pt 2 - solution

--- puzzles/Day 1/solution.py
+++ puzzles/Day 1/solution.py
@@ -3,6 +3,11 @@
 
 import pandas as pd
 
+
+####################
+######  pt 1  ######
+####################
+
 l1 = []
 l2 = []
 with open('advent-of-code-2024\puzzles\Day 1\input.txt') as f:
@@ -21,3 +26,17 @@ df = pd.DataFrame({'l1':l1,
 df['distance'] = abs(df['l1'] - df['l2'])
 
 print("Solution pt. 1 : ", df['distance'].sum())
+
+
+####################
+######  pt 2  ######
+####################
+
+df['l1_freq'] = df['l1'].map(df['l2'].value_counts())
+# df['l2_freq'] = df['l2'].map(df['l2'].value_counts())
+
+df['l1_sim'] = df['l1'] * df['l1_freq']
+# df['l2_sim'] = df['l2'] * df['l2_freq']
+
+# print(df)
+print("Solution pt. 2 : ", int(df['l1_sim'].sum()))
